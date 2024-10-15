c6a0c8af3b2034c0fab1058dea2209a2ec277e63
matheo.flores <matheoflores26@gmail.com>
2024-10-15 11:38:24 +0200

    ajout register/login

--- src/auth/register.php
+++ src/auth/register.php
@@ -1,27 +1,36 @@
 <?php
-    require_once('session_var.php');
-    require_once('header.php');
+    require_once('../session_var.php');
+    require_once('./functions_auth.php');
+    require_once('../header.php');
+    
+    if(!empty($_SESSION['currentUser'])){
+        header("Location: ../start.php");
+    }
+
+    if(!isset($errorMessage)) {
+        $errorMessage = NULL;
+    }
 
     function areInputsCompleted(){
         // Vérifiez d'abord si $_POST est défini avant d'accéder à ses clés
-        return isset($_POST['pseudo'], $_POST['mdp'], $_POST['mail']) && 
+        return isset($_POST['pseudo'], $_POST['password'], $_POST['email']) && 
             !empty($_POST['pseudo']) && 
-            !empty($_POST['mdp']) && 
-            !empty($_POST['mail']);
+            !empty($_POST['password']) && 
+            !empty($_POST['email']);
     }
 
     function findErrorMessage() {
         $verif = true;
         $pseudo = $_POST['pseudo'];
-        $mdp = $_POST['mdp'];
-        $mail = $_POST['mail'];
-        if(strlen($pseudo) < 4) {
-            $errorMessage = "Votre pseudo doit comporter au moins 4 caractères.";
+        $password = $_POST['password'];
+        $email = $_POST['email'];
+        if(strlen($pseudo)<4 && strlen($pseudo)>15 ) {
+            $errorMessage = "Votre pseudo doit comporter au moins 4 à 15 caractères.";
         }
-        elseif(strlen($mdp) < 6) {
-            $errorMessage = "Votre mot de passe doit comporter au moins 6 caractères.";
+        elseif(strlen($password) < 6 && strlen($password)>15) {
+            $errorMessage = "Votre mot de passe doit comporter au moins 6 à 15 caractères.";
         }
-        elseif(!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errorMessage = "L'adresse email n'a pas un format valide.";
         }
     }
@@ -29,17 +38,17 @@
     function areInputsVerified(&$errorMessage) {
         $verif = true;
         $pseudo = $_POST['pseudo'];
-        $mdp = $_POST['mdp'];
-        $mail = $_POST['mail'];
+        $password = $_POST['password'];
+        $email = $_POST['email'];
         if(strlen($pseudo) < 4) {
             $errorMessage = "Votre pseudo doit comporter au moins 4 caractères.";
             $verif = false;
         }
-        elseif(strlen($mdp) < 6) {
+        elseif(strlen($password) < 6) {
             $errorMessage = "Votre mot de passe doit comporter au moins 6 caractères.";
             $verif = false;
         }
-        elseif(!filter_var($mail, FILTER_VALIDATE_EMAIL)) {
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $errorMessage = "L'adresse email n'a pas un format valide.";
             $verif = false;
         }
@@ -52,24 +61,17 @@
             $connexion = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
             $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            // Vérification si le pseudo existe déjà
-            $sql = "SELECT * FROM utilisateur WHERE pseudo = :pseudo";
-            $stmt = $connexion->prepare($sql);
-            $stmt->bindParam(':pseudo', $_POST['pseudo']);
-            $stmt->execute();
-            $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-            if(!empty($res)) {
+            if(!empty(findUser($_POST['pseudo'],$connexion))) {
                 $errorMessage = "Ce pseudo est déjà emprunté.";    
             } else {
                 // Insérer le nouvel utilisateur
-                $sql = "INSERT INTO utilisateur (pseudo, mdp, mail) VALUES (:pseudo, :mdp, :mail)";
+                $sql = "INSERT INTO utilisateur (pseudo, password, email) VALUES (:pseudo, :password, :email)";
                 $stmt = $connexion->prepare($sql);
                 $stmt->bindParam(':pseudo', $_POST['pseudo']);
-                $stmt->bindParam(':mdp', $_POST['mdp']);
-                $stmt->bindParam(':mail', $_POST['mail']);
+                $stmt->bindParam(':password', $_POST['password']);
+                $stmt->bindParam(':email', $_POST['email']);
                 $stmt->execute();
-                echo "Utilisateur créé avec succès.";
+                header("Location: ./login.php");
             }
         } catch (PDOException $e) { 
             die('Erreur PDO : ' . $e->getMessage());
@@ -88,22 +90,28 @@
     <div class='container'>
         <h1>Page d'inscription</h1>
         <div class="error-message">
-            <?php echo $errorMessage ?>
+            <?php echo $errorMessage ;
+            $user = $_SESSION['currentUser'];
+            echo $user['pseudo'];?>
         </div>
         <form method='POST'>
             <label for="pseudo">Pseudo*
                 <input type="text" name="pseudo" REQUIRED>
             </label>
-            <label for="mail">Adresse mail*
-                <input type="text" name="mail" REQUIRED>
+            <label for="email">Adresse mail*
+                <input type="text" name="email" REQUIRED>
             </label>
-            <label for="mdp">Mot de passe*
-                <input type="text" name="mdp" REQUIRED>
+            <label for="password">Mot de passe*
+                <input type="text" name="password" REQUIRED>
             </label>
             <button type="submit">S'inscrire</button>
         </form>
+        <div id="redirection-div">
+            <p>Vous avez déjà un compte ? </p>
+            <a href="./login.php"> Se connecter</a>
+        </div>
     </div>
 
 <?php
-    require_once('./footer.php');
+    require_once('../footer.php');
 ?>
--- src/start.php
+++ src/start.php
@@ -3,7 +3,7 @@
     require_once('header.php');   
 ?>
     <?php
-    
+        
     ?>
 <?php
     require_once('./footer.php');
