c6e085d1be6d9df7eeb9943085c64796452556f6
CORNE Mathieu <mathieu.corne@etu.univ-lyon1.fr>
2024-10-15 11:58:02 +0200

    Amélioration du style + Article.php

--- article.php
+++ article.php
@@ -2,11 +2,40 @@
 <html>
     <head>
         <title>Sport News</title>
-        <link rel="stylesheet" href="./app.css">
+        <link rel="stylesheet" href="./app.css" />
+        <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet" />
         <?php 
+
+        function createComment() {
+            try {
+                $connexion = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+                $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                if (!(isset($_POST['currentUser']))) {
+                    $sql = "INSERT INTO comment(commentText, commentAuthor) VALUES (:commentText, :commentAuthor)";
+                    $stmt = $connexion->prepare($sql);
+                    $stmt->bindParam(':commentText', $_POST['commentText']);
+                    $stmt->bindParam(':commentAuthor', $_POST['currentUser']);
+                    $stmt->execute();
+                } else {
+                    header("Location: ./login.php");
+                }
+            } catch (PDOException $e) { 
+                die('Erreur PDO : ' . $e->getMessage());
+            } catch (Exception $e) {
+                die('Erreur Générale : ' . $e->getMessage());
+            }
+        }
 
         $article = [
             'title' => "Haaland débarque à Lyon",
+            'categories' => [
+                '0' => "OL",
+                '1' => "Ligue 1",
+                '2' => "France",
+                '3' => "Football",
+                '4' => "Mercato",
+            ],
             'creator' => "Kilian Mbappé"
         ];
 
@@ -19,17 +48,31 @@
     <body>
         <div class="article-container">
             <h1><?php echo $article['title']; ?></h1>
+            <div class="pastilles-container">
+                <?php 
+                    for ($i = 0; $i < sizeof($article["categories"]); $i++) {
+                        echo '<div class="pastille-category"><span>'.$article["categories"][strval($i)].'</span></div>';
+                    }
+                ?>
+            </div>
+            
             <div class="article-paragraphes-container">
                 <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
                 <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
                 <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
                 <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
             </div>
-            <p><?php echo $article['creator']; ?></p>
+            <p class="article-creator"><?php echo $article['creator']; ?></p><i class="fa-solid fa-xmark"></i>
         </div>
         <div class="section-separator"></div>
         <div class="commentaires-container">
             <h2>Commentaires</h2>
+            <form class="form-new-commentaire" method="POST">
+                <label class="new-commentaire-label" for="new-commentaire">Votre commentaire</label>
+                <textarea class="new-commentaire-text" name="new-commentaire"></textarea>
+                <a class="new-commentaire-login-button" href="./login.php">Se connecter</a>
+                <button type="submit" class="new-commentaire-submit-button">Envoyer le commentaire</button>
+            </form>
             <div class="commentaire-box">
                 <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
                 <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
@@ -38,6 +81,11 @@
                 <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
                 <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
             </div>
+            <div class="commentaire-box">
+                <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
+                <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
+            </div>
+            
         </div>
         <div class="section-separator"></div>
         <div class="page-switch-container">
