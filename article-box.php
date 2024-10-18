1652880cc5e0d118428ed47a09a49e45a2e5bd1c
Mathieu CORNE <math.corne@gmail.com>
2024-10-18 02:49:14 +0200

    Style (app.css), communication client-serveur pour la page article.php, ajout de quelques fonctions pour SELECT et INSERT des données

--- article-box.php
+++ article-box.php
@@ -1,7 +1,7 @@
 <div class="article-box">
-    <h2>Titre de l'article</h2>
-    <p class="article-box-interactions">5 Likes - 3 Commentaires</p>
-    <p class="article-box-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores mollitia. Inventore quod praesentium nobis fugit quas commodi possimus enim recusandae unde illo beatae, nostrum labore perspiciatis, optio pariatur consequuntur.</p>
-    <p class="article-box-date">Créé le 14 octobre 2024</p>
-    <p class="article-box-creator">Joe Doe</p>
+    <h2><?php echo $articleTitle?>Titre de l'article</h2>
+    <p class="article-box-interactions"><?php echo $nbLikes?> Likes - <?php echo $nbCommentaires?> Commentaires</p>
+    <p class="article-box-description"><?php echo $articleText?>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto, dolores mollitia. Inventore quod praesentium nobis fugit quas commodi possimus enim recusandae unde illo beatae, nostrum labore perspiciatis, optio pariatur consequuntur.</p>
+    <p class="article-box-date">Créé le 14 octobre 2024<?php echo $articleDate?></p>
+    <p class="article-box-creator"><?php echo $articleCreator?>Joe Doe</p>
 </div>
--- article.php
+++ article.php
@@ -4,88 +4,85 @@
         <title>Sport News</title>
         <link rel="stylesheet" href="./app.css" />
         <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet" />
-        <?php 
+        <?php
+        session_start();
 
-        function createComment() {
-            try {
-                $connexion = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-                $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        require "./src/functions.php";
+        $_SESSION["currentUser"] = 2;
 
-                if (!(isset($_POST['currentUser']))) {
-                    $sql = "INSERT INTO comment(commentText, commentAuthor) VALUES (:commentText, :commentAuthor)";
-                    $stmt = $connexion->prepare($sql);
-                    $stmt->bindParam(':commentText', $_POST['commentText']);
-                    $stmt->bindParam(':commentAuthor', $_POST['currentUser']);
-                    $stmt->execute();
-                } else {
-                    header("Location: ./login.php");
-                }
-            } catch (PDOException $e) { 
-                die('Erreur PDO : ' . $e->getMessage());
-            } catch (Exception $e) {
-                die('Erreur Générale : ' . $e->getMessage());
+        if (isset($_GET['id']) && is_numeric($_GET['id'])) {
+            $_SESSION["lastArticle"] = (int) $_GET['id'];;
+        } else {
+            die("Article non trouvé.");
+        }
+
+        if(isset($_POST['commentText']) && !empty($_POST['commentText'])) {
+            if (isset($_SESSION['commentText'])) {
+                unset($_SESSION['commentText']);
+            } else {
+                $_SESSION['commentText'] = $_POST['commentText'];
+            }
+            unset($_POST['commentText']);
+        }
+        
+        if (isset($_SESSION['currentUser'])) {
+            if (isset($_SESSION['commentText'])) {
+                createComment();
+                echo "Nouveau commentaire !";
             }
         }
 
-        $article = [
-            'title' => "Haaland débarque à Lyon",
-            'categories' => [
-                '0' => "OL",
-                '1' => "Ligue 1",
-                '2' => "France",
-                '3' => "Football",
-                '4' => "Mercato",
-            ],
-            'creator' => "Kilian Mbappé"
-        ];
 
-        $commentaire = [
-            'text' => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis illo exercitationem, impedit, nobis qui odit assumenda dolores rerum quae recusandae nisi molestiae unde excepturi natus commodi sequi consequuntur soluta. Molestiae?",
-            'creator' => "The Footix"
-        ];
+        /* Affichage des données de l'article*/
+        $article = selectArticle($_SESSION["lastArticle"]);
+        $creator = selectUser($article["id_creator"]);
+        $categories = selectCategories($_SESSION["lastArticle"]);
+        $paragraphes = selectParagraphs($_SESSION["lastArticle"]);
+        $comments = selectComments($_SESSION["lastArticle"]);
         ?>
     </head>
     <body>
+        <div class="navbar-container">
+            <a href="./article.php?id=0" class="navbar-option">Sport Live</a>
+            <a href="./login.php" class="navbar-option">Se connecter</a>
+        </div>
+        <div class="section-separator"></div>
         <div class="article-container">
-            <h1><?php echo $article['title']; ?></h1>
+            <h1><?php echo $article['titre'] ; ?></h1>
             <div class="pastilles-container">
                 <?php 
-                    for ($i = 0; $i < sizeof($article["categories"]); $i++) {
-                        echo '<div class="pastille-category"><span>'.$article["categories"][strval($i)].'</span></div>';
+                    for ($i = 0; $i < sizeof($categories); $i++) {
+                        echo '<div class="pastille-category"><span>'.$categories[strval($i)]["texte"].'</span></div>';
                     }
                 ?>
             </div>
             
             <div class="article-paragraphes-container">
-                <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
-                <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
-                <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
-                <p class="article-paragraphe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias modi consequatur voluptatum laudantium veniam nobis ut quo! Quia quae ratione debi tis ducimus modi similique, quod ex reprehenderit facilis cumque ipsa? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas maiores voluptate veritatis minima harum ex magnam tempora porro ratione dolorem, laboriosam corrupti facilis voluptates excepturi eius quod natus, aliquam ab?</p>
+                <?php 
+                    for ($i = 0; $i < sizeof($paragraphes); $i++) {
+                        echo '<p class="article-paragraphe">'.$paragraphes[strval($i)]["texte"].'</p>';
+                    }
+                ?>
             </div>
-            <p class="article-creator"><?php echo $article['creator']; ?></p><i class="fa-solid fa-xmark"></i>
+            <p class="article-creator"><?php echo $creator["pseudo"]; ?></p><i class="fa-solid fa-xmark"></i>
         </div>
         <div class="section-separator"></div>
         <div class="commentaires-container">
             <h2>Commentaires</h2>
             <form class="form-new-commentaire" method="POST">
-                <label class="new-commentaire-label" for="new-commentaire">Votre commentaire</label>
-                <textarea class="new-commentaire-text" name="new-commentaire"></textarea>
-                <a class="new-commentaire-login-button" href="./login.php">Se connecter</a>
-                <button type="submit" class="new-commentaire-submit-button">Envoyer le commentaire</button>
+                <label class="new-commentaire-label" for="commentText">Votre commentaire</label>
+                <textarea class="new-commentaire-text" name="commentText"></textarea>
+                
+                <button type="submit" class="new-commentaire-submit-button"><?php echo (empty($_SESSION["currentUser"]) ? "Se connecter" : "Envoyer le commentaire"); ?></button>
             </form>
-            <div class="commentaire-box">
-                <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
-                <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
-            </div>
-            <div class="commentaire-box">
-                <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
-                <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
-            </div>
-            <div class="commentaire-box">
-                <p class="commentaire-text"><?php echo $commentaire['text']; ?><p>
-                <p class="commentaire-creator"><?php echo $commentaire['creator']; ?></p>
-            </div>
+            <?php 
+                for ($i = 0; $i < sizeof($comments); $i++) {
+                    echo '<div class="commentaire-box"><p class="commentaire-text">'.$comments[strval($i)]["texte"].'</p><p class="commentaire-creator">'.selectUser($comments[strval($i)]["id_creator"])["pseudo"].'</p></div>';
+                }
+            ?>
+            <?php 
             
+            ?>
         </div>
         <div class="section-separator"></div>
         <div class="page-switch-container">
